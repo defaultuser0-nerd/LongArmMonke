f091d12395c2e2a83d13f354f062e336f6ece32a
defaultuser0 <axolotlslmao@gmail.com>
2025-03-16 21:23:11 -0500

    EVEN MORE EXPLODE CUS I FORGOT!!!!

---
+++ Mod.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Photon.Voice.Unity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongarmMonke
+{
+    [BepInPlugin("default.LongArmMonke", "LongArmMonke", "1.0.1")]
+    public class Mod : BaseUnityPlugin 
+    {
+        public static ConfigEntry<bool> Arms;
+
+        void Start() =>
+            Arms = Config.Bind("Settings", "LongArms Enabled", false);
+    }
+}
